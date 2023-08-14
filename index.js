93864a93d0e36275934c57971c84b14b9635a60c
nomadleperchaun <100307446+nomadleperchaun@users.noreply.github.com>
2023-08-14 10:42:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navlinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		document.body.classList.remove('nav-open');
+	})
+})
+
+//contact form
